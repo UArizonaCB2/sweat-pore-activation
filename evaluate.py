95f0a7ddf031fc316afa32135cab29d1251ffaaf
Ricky Su <hanyusu@arizona.edu>
2024-08-07 16:29:45 -0700

    stratified_KFold_loader

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,8 @@
 import os, argparse, torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
 from CNNs import SimpleCNN_p32, SimpleCNN_p17
 from PIL import Image
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from torchvision import transforms
 import torch.nn.functional as F
 from torchvision.utils import save_image
@@ -114,119 +114,154 @@ class algorithm:
     # Use train-test-split
     # Split the data -- Train Validate Test
     test_loader = DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 0)
-
-    def evaluateModel(test_loader, device, model):
-        model = model.to(device)
+    
+    def stratified_KFold_loader(dataset, n_splits=10, batch_size = batchSize):
+        """
+        This function is going to return DataLoader with 
+        stratified_Kfold to get the distributed data
+        """
+        print(dataset[0], f'Length of dataset: {len(dataset)}')
+        # Extract data and labels
+        data = []
+        labels = []
+        for info in dataset:
+            data.append(info[0])
+            labels.append(info[1])
+            
+        SKF = StratifiedKFold(n_splits)
         
-        fp_names = [] # Image names for false.
-        fn_names = [] # Images where we missed predicting the pore.
-        tp_names = []
-        tn_names = []
-        confusionMatric = { # Dict of confusion matix
-            "TP: ":0,
-            "TN: ":0,
-            "FP: ":0,
-            "FN: ":0}
+        train_loader = None
+        test_loader = None
+        for fold, (train_indices, test_indices) in enumerate(SKF.split(data, labels)):
+            # Create Subset datasets
+            train_dataset = Subset(dataset, train_indices)
+            test_dataset = Subset(dataset, test_indices)
 
-        TP = 0
-        TN = 0
-        FP = 0
-        FN = 0
+            # Create DataLoaders
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-        # disable gradient calculation
-        with torch.no_grad():
-            for images, labels, names in test_loader:
-                images, labels = images.to(device), labels.to(device)
+            print(f"Fold {fold + 1}:")
+            print(f"  Train set size: {len(train_dataset)}")
+            print(f"  Test set size: {len(test_dataset)}")
+        
+        return train_loader, test_loader
+    
+    train_loader, test_loader = stratified_KFold_loader(dataset, n_splits=10, batch_size = batchSize)
+    
+
+    # def evaluateModel(test_loader, device, model):
+    #     model = model.to(device)
+        
+    #     fp_names = [] # Image names for false.
+    #     fn_names = [] # Images where we missed predicting the pore.
+    #     tp_names = []
+    #     tn_names = []
+    #     confusionMatric = { # Dict of confusion matix
+    #         "TP: ":0,
+    #         "TN: ":0,
+    #         "FP: ":0,
+    #         "FN: ":0}
+
+    #     TP = 0
+    #     TN = 0
+    #     FP = 0
+    #     FN = 0
+
+    #     # disable gradient calculation
+    #     with torch.no_grad():
+    #         for images, labels, names in test_loader:
+    #             images, labels = images.to(device), labels.to(device)
                 
-                prediction = model(images)
+    #             prediction = model(images)
 
-                # Apply softmax to convert the raw logits into probabilities
-                probabilities = F.softmax(prediction, dim=1)
+    #             # Apply softmax to convert the raw logits into probabilities
+    #             probabilities = F.softmax(prediction, dim=1)
 
-                # Finds the highest probabilities for each image
-                _, predicted_classes = torch.max(probabilities, 1)
+    #             # Finds the highest probabilities for each image
+    #             _, predicted_classes = torch.max(probabilities, 1)
 
-                for i in range(len(labels)):
-                    if predicted_classes[i].item() == labels[i].item() == 1:
-                        # Create the full path for the image file
-                        save_dir = f'results/TP/'
-                        filename = names[i]
-                        full_path = os.path.join(save_dir, filename)
-                        # Save the image
-                        save_image(images[0], full_path)
+    #             for i in range(len(labels)):
+    #                 if predicted_classes[i].item() == labels[i].item() == 1:
+    #                     # Create the full path for the image file
+    #                     save_dir = f'results/TP/'
+    #                     filename = names[i]
+    #                     full_path = os.path.join(save_dir, filename)
+    #                     # Save the image
+    #                     save_image(images[0], full_path)
                     
-                        tp_names.append(names[i])
-                        TP += 1
-                    elif predicted_classes[i].item() == labels[i].item() == 0:
-                        # Create the full path for the image file
-                        save_dir = f'results/TN/'
-                        filename = names[i]
-                        full_path = os.path.join(save_dir, filename)
-                        # Save the image
-                        save_image(images[0], full_path)
-                        tn_names.append(names[i])
-                        TN += 1
-                    elif predicted_classes[i].item() != labels[i].item() and predicted_classes[i].item() == 1:
-                        # Create the full path for the image file
-                        save_dir = f'results/FP/'
-                        filename = names[i]
-                        full_path = os.path.join(save_dir, filename)
-                        # Save the image
-                        save_image(images[0], full_path)
-                        fp_names.append(names[i]) # Keep track of the fp
-                        FP += 1
-                    else:
-                        # Create the full path for the image file
-                        save_dir = f'results/FN/'
-                        filename = names[i]
-                        full_path = os.path.join(save_dir, filename)
-                        # Save the image
-                        save_image(images[0], full_path)
-                        fn_names.append(names[i]) # Keep track of the fn
-                        FN += 1
+    #                     tp_names.append(names[i])
+    #                     TP += 1
+    #                 elif predicted_classes[i].item() == labels[i].item() == 0:
+    #                     # Create the full path for the image file
+    #                     save_dir = f'results/TN/'
+    #                     filename = names[i]
+    #                     full_path = os.path.join(save_dir, filename)
+    #                     # Save the image
+    #                     save_image(images[0], full_path)
+    #                     tn_names.append(names[i])
+    #                     TN += 1
+    #                 elif predicted_classes[i].item() != labels[i].item() and predicted_classes[i].item() == 1:
+    #                     # Create the full path for the image file
+    #                     save_dir = f'results/FP/'
+    #                     filename = names[i]
+    #                     full_path = os.path.join(save_dir, filename)
+    #                     # Save the image
+    #                     save_image(images[0], full_path)
+    #                     fp_names.append(names[i]) # Keep track of the fp
+    #                     FP += 1
+    #                 else:
+    #                     # Create the full path for the image file
+    #                     save_dir = f'results/FN/'
+    #                     filename = names[i]
+    #                     full_path = os.path.join(save_dir, filename)
+    #                     # Save the image
+    #                     save_image(images[0], full_path)
+    #                     fn_names.append(names[i]) # Keep track of the fn
+    #                     FN += 1
                         
-        confusionMatric["TP: "] = TP
-        confusionMatric["FP: "] = FP
-        confusionMatric["TN: "] = TN
-        confusionMatric["FN: "] = FN
+    #     confusionMatric["TP: "] = TP
+    #     confusionMatric["FP: "] = FP
+    #     confusionMatric["TN: "] = TN
+    #     confusionMatric["FN: "] = FN
                 
-        return fp_names, fn_names, tn_names, tp_names, [TP, TN, FP, FN]
+    #     return fp_names, fn_names, tn_names, tp_names, [TP, TN, FP, FN]
     
-    fp, fn, tn, tp, results= evaluateModel(test_loader, device, trainedModel)
+    # fp, fn, tn, tp, results= evaluateModel(test_loader, device, trainedModel)
     
-    def ConfusionMatrix(results, modelName):
-        TP, TN, FP, FN = results
+    # def ConfusionMatrix(results, modelName):
+    #     TP, TN, FP, FN = results
 
-        print("--- Confusion Matrix ---\n")
-        print(f"Model: {modelName}\n")
-        print(f"TP:{TP}, TN:{TN}, FP:{FP}, FN:{FN}\n")
+    #     print("--- Confusion Matrix ---\n")
+    #     print(f"Model: {modelName}\n")
+    #     print(f"TP:{TP}, TN:{TN}, FP:{FP}, FN:{FN}\n")
         
-        if TP + FP + FN + TN > 0:
-            accuracy = (TP + TN) / (TP + FP + FN + TN)
-            print(f"Accuracy: {accuracy:.4f}\n")
-        else:
-            print("Accuracy: N/A\n")
+    #     if TP + FP + FN + TN > 0:
+    #         accuracy = (TP + TN) / (TP + FP + FN + TN)
+    #         print(f"Accuracy: {accuracy:.4f}\n")
+    #     else:
+    #         print("Accuracy: N/A\n")
         
-        if TP + FP > 0:
-            precision = TP / (TP + FP)
-            print(f"Precision: {precision:.4f}\n")
-        else:
-            print("Precision: N/A (no positive predictions)\n")
+    #     if TP + FP > 0:
+    #         precision = TP / (TP + FP)
+    #         print(f"Precision: {precision:.4f}\n")
+    #     else:
+    #         print("Precision: N/A (no positive predictions)\n")
 
-        if TP + FN > 0:
-            recall = TP / (TP + FN)
-            print(f"Recall: {recall:.4f}\n")
-        else:
-            print("Recall: N/A (no actual positive samples)\n")
+    #     if TP + FN > 0:
+    #         recall = TP / (TP + FN)
+    #         print(f"Recall: {recall:.4f}\n")
+    #     else:
+    #         print("Recall: N/A (no actual positive samples)\n")
 
-        if precision + recall > 0:
-            f1 = 2 * (precision * recall) / (precision + recall)
-            print(f"F-score: {f1:.4f}\n")
-        else:
-            print("F-score: N/A (precision and recall are both zero)\n")
-        return 
+    #     if precision + recall > 0:
+    #         f1 = 2 * (precision * recall) / (precision + recall)
+    #         print(f"F-score: {f1:.4f}\n")
+    #     else:
+    #         print("F-score: N/A (precision and recall are both zero)\n")
+    #     return 
     
-    ConfusionMatrix(results, cnn_name)
+    # ConfusionMatrix(results, cnn_name)
     
     
 
