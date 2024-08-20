9b2b7e7537f5421a93975a0f3ced513b7fcdcc2e
Ricky Su <hanyusu@arizona.edu>
2024-08-20 15:56:34 -0700

    create a dataset folder in Preprocessing

--- Preprocessing/img_segmentation/prepareDataset.py
+++ Preprocessing/img_segmentation/prepareDataset.py
@@ -1,2 +1,89 @@
-if __name__ == "main":
-    print("prepare dataset")
+from torch.utils.data import Dataset, Subset
+import torch.nn.functional as F
+import os, argparse, torch
+from PIL import Image
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--patchSize',
+                    type=int)
+
+parser.add_argument('--TrainingPercentage',
+                    default=0.8,
+                    type=float)
+
+parser.add_argument('--TestingPercentage',
+                    default=0.2,
+                    type=float)
+
+args = parser.parse_args()
+
+class SweatPoresDataset(Dataset):
+    def __init__(self, img_dir, transforms = None):
+        """
+        It is run once when instantiating the Dataset object. 
+        """
+        self.img_dir = img_dir
+        self.transform = transforms
+        self.img_files = os.listdir(img_dir) # list of image files
+
+    def __len__(self):
+        """
+        It returns the number of samples in our dataset
+        """
+        return len(self.img_files)
+
+    def __getitem__(self, idx):
+        """
+        It loads and returns a smaple from the dataset at the given index
+        """
+        img_path = os.path.join(self.img_dir, self.img_files[idx])
+        image = Image.open(img_path).convert('RGB') # Retrieve the image data 
+        label = int(self.img_files[idx].split('_')[-1].split('.')[0]) # Etrack the label from the image
+        img_name = self.img_files[idx] 
+
+        if self.transform: 
+            image = self.transform(image)
+
+        return image, label, img_name
+
+if __name__ == "__main__":
+    print("-- Dataset Info --")
+    patchSize = args.patchSize
+    train_size = args.TrainingPercentage
+    test_size = args.TestingPercentage
+    # Build the custom dataset
+    
+    # Create Transformation
+    """
+    Converting Images to Tensor and normalize the pixel values
+    """
+    trans = transforms.Compose([
+        transforms.Grayscale(),  # Convert RGB to grayscale 3 ---> 1 
+        transforms.ToTensor(), # Convert data to Tensor for dataloader
+        transforms.Normalize(mean=0.5, std=0.5) # scale the pixel values of the image between -1 and 1
+    ])
+    
+    # Apply transformation on the dataset 
+    datadir = f'Preprocessing/output_patches/patch_size/{patchSize}X{patchSize}'
+    dataset = SweatPoresDataset(img_dir = datadir, transforms = trans)
+    
+    # Split the data indices -- Train Validate Test
+    train_indices, test_indices = train_test_split(range(len(dataset)), test_size=test_size, train_size=train_size)
+    
+    # Subset objects can be directly used in Dataloader
+    trainIndices = Subset(dataset, train_indices)
+    testIndices = Subset(dataset, test_indices)
+    
+    # Save these dataset indices for memory efficiency
+    torch.save(dataset, f'Preprocessing/dataset/dataset.pt')
+    torch.save(trainIndices, f'Preprocessing/dataset/train_indices.pt')
+    torch.save(testIndices, f'Preprocessing/dataset/test_indices.pt')
+    
+    
+    # Testing out the dataset 
+    print(f"Total data: {len(dataset)}")
+    print(f"Training dataset: {len(train_indices)}")
+    print(f"Testing dataset: {len(test_indices)}")
--- scripts/runPreprocessing.sh
+++ scripts/runPreprocessing.sh
@@ -7,6 +7,7 @@ coordinatesDir='Preprocessing/output_patches/centroid_coordinates'
 centroidsDir='Preprocessing/output_patches/contour_images'
 patchesDir='Preprocessing/output_patches/patch_size'
 
+patch_size=32
 
 python Preprocessing/img_segmentation/getPatches.py --patchSize 32 --rawDir "$inputDirRaw" --annotatedDir "$inputDirAnnotated" --coordinatesDir "$coordinatesDir" --centroidsDir "$centroidsDir" --patchesDir "$patchesDir"
-# python Preprocessing/img_segmentation/prepareDataset.py
+python Preprocessing/img_segmentation/prepareDataset.py --patchSize $patch_size  --TrainingPercentage 0.8 --TestingPercentage 0.2
