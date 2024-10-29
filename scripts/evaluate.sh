0dffbc471c618f2dfbc709045b5316d2694bffe1
Ricky Su <hanyusu@arizona.edu>
2024-10-28 17:20:28 -0700

    modify the echo

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -5,7 +5,7 @@ imgName="6" #ex. 2.bmp ---> 2
 patchesDir="Preprocessing/centralizedPatches/32X32/"
 
 # 1. Run the preprocessing step
-echo "Running preprocessing to create prediction on the unseen patches..."
+echo "Running preprocessing to create prediction on the unseen centralized pore patches..."
 rm -rf Preprocessing/centralizedPatches/32X32/* #Delete the existing patches 
 python Preprocessing/centralize_sweatpores.py --imgName $imgName
 # 2. Run the evaluation
