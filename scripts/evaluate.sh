67c955879477676c2f53b9bf0186b3791b11b56a
Ricky Su <hanyusu@arizona.edu>
2025-01-28 11:03:03 -0700

    trained and compare models

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,7 +3,7 @@ chmod +x ./scripts/evaluate.sh # Excute permission
 
 # Golbal variables
 centralized=false  # true: centralized pore pathces, false: sliding window pathces
-imgName="6" #ex. 2.bmp ---> 2
+imgName="2" #ex. 2.bmp ---> 2
 
 if [ "$centralized" = true ]; then
     patchesDir="Preprocessing/centralizedPatches/32X32/"
@@ -17,9 +17,10 @@ if [ "$centralized" = true ]; then
     python evaluate.py --batchSize 8 --CNNmodel 'CNN4Layers_p32_e20_135bmps.model' --prediction $imgName'bmp' --device 'mps' --patchesDir $patchesDir
 else
     echo "Running preprocessing to create prediction on the unseen sliding window patches..."
-    patchesDir="Preprocessing/testingModel_output_patches/${imgName}bmp/patch_size/32X32/"
-
+    # patchesDir="Preprocessing/testingModel_output_patches/${imgName}bmp/patch_size/32X32/"
+    patchesDir="Preprocessing/testingModel_output_patches/bmps/patch_size/32X32/"
     #Run the evaluation
     echo "Starting evaluation..."
-    python evaluate.py --batchSize 8 --CNNmodel 'CNN4Layers_p32_e20_135bmps.model' --prediction $imgName'bmp' --device 'mps' --patchesDir $patchesDir
+    #python evaluate.py --batchSize 8 --CNNmodel 'CNN4Layers_p32_e20_135bmps.model' --prediction $imgName'bmp' --device 'mps' --patchesDir $patchesDir
+    python evaluate.py --batchSize 8 --CNNmodel 'CNN4LayersV4_p32_e25_noFold.model' --prediction $imgName'bmp' --device 'mps' --patchesDir $patchesDir
 fi
