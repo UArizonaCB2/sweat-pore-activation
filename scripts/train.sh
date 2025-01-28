d54cec4c8829acda248ab5f3606bc8924659a627
Ricky Su <hanyusu@arizona.edu>
2025-01-28 12:20:14 -0700

    retrained 3 models and get the evaluation on the best one

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,4 +2,4 @@ chmod +x ./scripts/train.sh # Excute permission
 
 # The user can add --tag 'info' for naming the ML model (defualt is None)
 # python train.py --batchSize 8 --CNN 'CNN4LayersV4_p32' --epochs 25 --device 'mps'
-python train.py --batchSize 8 --CNN 'CNN4LayersV4_p32' --epochs 25 --device 'mps'
+python train.py --batchSize 8 --CNN 'SimpleCNN_p32' --epochs 25 --device 'mps'
